0bfc82aa302dcf64774d1539625594951afaea7f
Joshua <ytcho@devguru.co.kr>
2021-04-27 17:52:44 +0900

    first commit

---
+++ cmakeTest/cmakeTest.cpp
@@ -0,0 +1,14 @@
+﻿// cmakeTest.cpp : 애플리케이션의 진입점을 정의합니다.
+//
+
+#include "cmakeTest.h"
+#include "testCode.cpp"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	test_print();
+	return 0;
+}
---
+++ cmakeTest/testCode.cpp
@@ -0,0 +1,7 @@
+using namespace std;
+
+int test_print()
+{
+	cout << "Hello Joshua." << endl;
+	return 0;
+}
